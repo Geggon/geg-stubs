882237b79e33f3838a6476d06b1d69d3760bfa6e
Geggon <120031368+Geggon@users.noreply.github.com>
2024-04-11 23:19:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Test'
+LONG_DESCRIPTION = 'Stubs.'
 
 # Setting up
 setup(
