2253f884ec1c8efdb19193cbe46d35387c27bbe3
Geggon <120031368+Geggon@users.noreply.github.com>
2024-04-11 23:28:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='geg-stubs',
     version='1.0.0',
-    description='Tools for setting up stubs and mocks.',
-    author='Geg',
-    author_email='emtastan@gmx.de',
-    url='http://github.com/Geggon/geg-stubs',
-    license='MIT License',
-    keywords='test',
+    description='test',
     packages=find_packages(),
-    classifiers=(
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ),
 )
